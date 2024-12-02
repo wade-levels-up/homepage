d492d0df096896200fcab9d0adc3e9c7dd37f9ff
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-12-02 19:39:05 +1100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|avif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: 'defaults',
+            presets: [['@babel/preset-env']],
+          },
+        },
+      },
+    ],
+  },
+};
